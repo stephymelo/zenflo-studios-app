ab3f57f78c2d57b8acac63bc489645b375ba7563
stephym <stephymelo99@gmail.com>
2023-07-12 20:58:52 -0500

    organizing

---
+++ src/Components/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Landing.css';
+
+interface Landing {
+}
+
+export const Landing: React.FC<Landing> = () => {
+    return (
+<section className='Landing'>
+</section>
+);
+}
